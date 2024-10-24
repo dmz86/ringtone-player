c4580a48729a5f0ea6d1d9d360abb85becbed234
OSX <m.domiziani@meditrial.eu>
2024-10-24 11:03:38 +0200

    update method

--- android/src/main/java/com/getcapacitor/community/ringtone/Ringotone.java
+++ android/src/main/java/com/getcapacitor/community/ringtone/Ringotone.java
@@ -3,19 +3,20 @@ package com.getcapacitor.community.ringtone;
 import android.util.Log;
 
 public class Ringotone {
+    String TAG = "Ringotone";
 
     public String echo(String value) {
-        Log.i("Echo", value);
+        Log.i(TAG, "echo");
         return value;
     }
 
     public Boolean playRingtone() {
-
+        Log.i(TAG, "playRingtone");
         return true;
     }
 
     public Boolean stopRingtone() {
-
+        Log.i(TAG, "stopRingtone");
         return true;
     }
 }
--- android/src/main/java/com/getcapacitor/community/ringtone/RingotonePlugin.java
+++ android/src/main/java/com/getcapacitor/community/ringtone/RingotonePlugin.java
@@ -1,5 +1,11 @@
 package com.getcapacitor.community.ringtone;
 
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -11,10 +17,12 @@ public class RingotonePlugin extends Plugin {
 
     private Ringotone implementation = new Ringotone();
 
+    String TAG = "RingotonePlugin";
+
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
-
+        Log.i(TAG, "echo");
         JSObject ret = new JSObject();
         ret.put("value", implementation.echo(value));
         call.resolve(ret);
@@ -22,21 +30,21 @@ public class RingotonePlugin extends Plugin {
 
     @PluginMethod
     public void playRingtone(PluginCall call) {
-        return;
-//        try {
-//            Context context = getContext();
-//
-//            // Ottieni la suoneria di default del dispositivo
-//            Uri ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-//            Ringtone ringtone = RingtoneManager.getRingtone(context, ringtoneUri);
-//
-//            // Riproduci la suoneria
-//            ringtone.play();
-//
-//            call.resolve();
-//        } catch (Exception e) {
-//            call.reject("Errore nella riproduzione della suoneria", e);
-//        }
+        Log.i(TAG, "playRingtone");
+        try {
+            Context context = getContext();
+
+            // Ottieni la suoneria di default del dispositivo
+            Uri ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            Ringtone ringtone = RingtoneManager.getRingtone(context, ringtoneUri);
+
+            // Riproduci la suoneria
+            ringtone.play();
+
+            call.resolve();
+        } catch (Exception e) {
+            call.reject("Errore nella riproduzione della suoneria", e);
+        }
     }
     @PluginMethod
     public void stopRingtone(PluginCall call) {
